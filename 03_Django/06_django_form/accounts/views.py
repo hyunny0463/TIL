e49dc3c68d8dbeb372dafad5cdd52a00094412a9
Seohyun Baek <hyunny0463@gmail.com>
2019-10-24 18:37:25 +0900

    social login

--- 03_Django/06_django_form/accounts/views.py
+++ 03_Django/06_django_form/accounts/views.py
@@ -34,7 +34,7 @@ def login(request):
     else:
         form = AuthenticationForm()
     context = {'form': form, }
-    return render(request, 'accounts/auth_form.html', context)
+    return render(request, 'accounts/login.html', context)
 
 def logout(request):
     auth_logout(request)
